f665e7904bcbaa6e389a802bc834ea0a6788778f
JamesJhong <jamesjhong@star-link.tech>
2025-08-17 06:56:26 +0800

    feat: 🎸 add provider

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject, Optional } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('AUTHOR') private readonly author: string,
+    @Inject('AUTHOR_MESSAGE') private readonly authorMessage: string,
+    @Inject('ALIAS_APP_SERVICE') private readonly aliasAppService: AppService,
+    @Inject('CUSTOM_MESSAGE') private readonly customMessage: string,
+    @Optional() @Inject('NO_EXIST') private readonly noExist: unknown,
+  ) {
+    console.log(this.appService === this.aliasAppService);
+    console.log('AUTHOR', this.author);
+    console.log('AUTHOR_MESSAGE', this.authorMessage);
+    console.log('CUSTOM_MESSAGE', this.customMessage);
+    console.log('NO_EXIST', this.noExist);
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessageModule } from './module/message/message.module';
 
 @Module({
-  imports: [],
+  imports: [MessageModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: AppService,
+      useClass: AppService,
+    },
+    {
+      provide: 'AUTHOR',
+      useValue: 'HAO',
+    },
+    {
+      provide: 'AUTHOR_MESSAGE',
+      inject: [AppService],
+      useFactory: (appService: AppService) => {
+        const hello = appService.getHello();
+        return `HAO: ${hello}`;
+      },
+    },
+    {
+      provide: 'ALIAS_APP_SERVICE',
+      useExisting: AppService,
+    },
+  ],
 })
 export class AppModule {}
