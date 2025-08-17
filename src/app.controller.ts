fb00105da1ca9968ad326a6dbd317d47b1a84e12
JamesJhong <jamesjhong@star-link.tech>
2025-08-17 08:15:17 +0800

    feat: 🎸 add filter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Get()
+  getHello(): string {
+    const obj = {
+      code: HttpStatus.BAD_REQUEST,
+      msg: '錯誤拉',
+    };
+    throw new HttpException(obj, HttpStatus.BAD_REQUEST);
+  }
+
+  @Get('/test-bad-request-class')
+  getBadRequestClass() {
+    const obj = {
+      code: HttpStatus.BAD_REQUEST,
+      message: '錯誤拉',
+      xxx: 'xxx',
+    };
+    throw new BadRequestException(obj);
+  }
+
+  @Get('/test-bad-request-class1')
+  getBadRequestClass1() {
+    throw new HttpException('錯誤!!', HttpStatus.BAD_REQUEST);
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpFilter } from './filter/http/http.filter';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpFilter,
+    },
+  ],
 })
 export class AppModule {}
