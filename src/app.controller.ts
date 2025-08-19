2b0f34a1d2fdcebd0c67a562b97a9ee9ddb31c51
JamesJhong <jamesjhong@star-link.tech>
2025-08-19 09:38:06 +0800

    feat(custom-decorator): add custom-decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Roles } from './decorators/roles/roles.decorator';
+import { RoleGuard } from './guards/role/role.guard';
+import { User } from './decorators/user/user.decorator';
+import { Authorization } from './decorators/authorization/authorization.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  // @UseGuards(RoleGuard)
+  // @Roles('admin', 'staff')
+  @Authorization('admin')
+  @Get()
+  getHello(@User('xxx') xxx: string): string {
+    return xxx;
+  }
 }
---
+++ src/middlewares/mock-data/mock-data.middleware.ts
@@ -0,0 +1,13 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+
+@Injectable()
+export class MockDataMiddleware implements NestMiddleware {
+  use(req: any, res: any, next: () => void) {
+    req.role = 'staff';
+    req.user = {
+      name: 'HAO',
+      xxx: 'yo yo yo',
+    };
+    next();
+  }
+}
