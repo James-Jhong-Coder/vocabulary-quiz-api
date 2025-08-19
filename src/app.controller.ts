41e4eb0b439cabd9972a51706ef8b2d6ae393d50
JamesJhong <jamesjhong@star-link.tech>
2025-08-19 10:46:08 +0800

    feat(life-cycle): test life cycle

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Scope } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({
+  path: 'test',
+  scope: Scope.REQUEST, // 每次請求會新建一次
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello() {
+    console.log('[AppController] Handling request...');
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  OnApplicationBootstrap,
+  OnApplicationShutdown,
+} from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class AppService
+  implements
+    OnModuleInit,
+    OnModuleDestroy,
+    OnApplicationBootstrap,
+    OnApplicationShutdown
+{
   getHello(): string {
     return 'Hello World!';
+  }
+
+  onModuleInit() {
+    console.log('[AppService] onModuleInit');
+  }
+
+  onModuleDestroy() {
+    console.log('[AppService] onModuleDestroy');
+  }
+
+  onApplicationBootstrap() {
+    console.log('[AppService] onApplicationBootstrap');
+  }
+
+  onApplicationShutdown(signal?: string) {
+    console.log('[AppService] onApplicationShutdown', signal);
   }
 }
